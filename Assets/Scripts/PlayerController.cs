f408c8751727e3bb2c4ad08cb59a2cb9110c5ff0
SManush <manushsimonyan@gmail.com>
2024-10-11 12:37:10 +0400

    - Score pregress save system.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class PlayerController : MonoBehaviour
 {
@@ -21,6 +23,15 @@ public class PlayerController : MonoBehaviour
     public GameObject gameOverText;
     private ScoreManager scoreManager;
 
+    private void Awake()
+    {
+        if (Application.persistentDataPath + "/savefile.json" != null)
+        {
+            UpdateScoreUI();
+            LoadScore();
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +40,10 @@ public class PlayerController : MonoBehaviour
         playerAudio = GetComponent<AudioSource>();
         scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
         scoreManager = FindObjectOfType<ScoreManager>();
+        SaveScore();
+        Debug.Log(Application.persistentDataPath);
+        LoadScore();
+        UpdateScoreUI();
     }
 
     // Update is called once per frame
@@ -37,6 +52,12 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        
+        SaveScore();
+        LoadScore();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Obstacle"))
@@ -74,5 +95,49 @@ public class PlayerController : MonoBehaviour
                 lastTriggerTime = Time.time;
             }
         }
+    }
+    [System.Serializable]
+    class SaveData
+    {
+        public int score;
+    }
+
+    public void SaveScore()
+    {
+        Debug.Log("Saving Score: " + score); 
+
+        SaveData data = new SaveData();
+        data.score = score; // Saving score
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+
+        Debug.Log("Score saved successfully");
+    }
+    public void LoadScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path); // Read from JSON file
+            SaveData data = JsonUtility.FromJson<SaveData>(json); // Pars JSON
+
+            score = data.score; // Loading saved scores
+            scoreText.text = score.ToString();
+            Debug.Log("Score loaded: " + score);
+        }
+        else
+        {
+            Debug.Log("Save file not found!");
+        }
+    }
+    void OnApplicationQuit()
+    {
+        SaveScore(); 
+    }
+    void UpdateScoreUI()
+    {
+        scoreText.text = score.ToString();
     }
 }
